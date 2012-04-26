243459de2cd6473d564f1c5387c5d8e500e396e2
race604 <race604@gmail.com>
2012-04-26 17:32:37 +0800

    init the repo

---
+++ src/com/race604/fetion/FetionActivity.java
@@ -0,0 +1,13 @@
+package com.race604.fetion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FetionActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
